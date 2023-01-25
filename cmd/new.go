d5ab6e1af39fce1626260374e771e1f0a310d546
David McLeish <dave@mcleish.id.au>
2023-01-25 22:48:33 +1100

    new command

--- cmd/new.go
+++ cmd/new.go
@@ -1,1 +1,79 @@
 package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	prefix      string
+	title       string
+	description string
+
+	newCmd = &cobra.Command{
+		Use: "new",
+		Run: runNew,
+	}
+)
+
+func init() {
+	newCmd.Flags().StringVar(&prefix, "prefix", "MOP", "ticket ID prefix")
+	newCmd.Flags().StringVar(&title, "title", "", "ticket title")
+	newCmd.Flags().StringVar(&description, "description", "", "ticket description")
+	rootCmd.AddCommand(newCmd)
+}
+
+type Ticket struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Prefix      string             `bson:"prefix"`
+	Num         int                `bson:"num"`
+	Title       string             `bson:"title"`
+	Description string             `bson:"description"`
+}
+
+func runNew(cmd *cobra.Command, args []string) {
+	withMongo(cmd, func(ctx context.Context, client *mongo.Client) error {
+		tickets := client.Database("mopoke").Collection("tickets")
+
+		session, err := client.StartSession()
+		if err != nil {
+			return err
+		}
+		defer session.EndSession(ctx)
+
+		result, err := session.WithTransaction(ctx,
+			func(sessCtx mongo.SessionContext) (interface{}, error) {
+				var max Ticket
+				opts := options.FindOne().SetSort(bson.D{{"num", -1}})
+				maxResult := tickets.FindOne(sessCtx, bson.D{{"prefix", prefix}}, opts)
+				if err := maxResult.Decode(&max); err != nil {
+					// Not found?
+					log.Println(err)
+				}
+				ticket := Ticket{
+					Prefix:      prefix,
+					Num:         max.Num + 1,
+					Title:       title,
+					Description: description,
+				}
+				fmt.Printf("%#v\n", ticket)
+				insertResult, err := tickets.InsertOne(sessCtx, ticket)
+				if err != nil {
+					return nil, err
+				}
+				return insertResult, nil
+			})
+		if err != nil {
+			return err
+		}
+		fmt.Println(result)
+		return nil
+	})
+}
