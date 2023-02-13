e0261fd39f0e5240548ddf72651e4948b95cb4fb
David McLeish <dave@mcleish.id.au>
2023-02-13 21:19:17 +1100

    db package

---
+++ db/ticket.go
@@ -0,0 +1,22 @@
+package db
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Ticket struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Name        string             `bson:"name"`
+	Title       string             `bson:"title"`
+	Description string             `bson:"description"`
+
+	Rel      []Rel    `bson:"rel"`      // aggregated
+	Children []Ticket `bson:"children"` // aggregated
+}
+
+type Rel struct {
+	From primitive.ObjectID `bson:"from"`
+	To   primitive.ObjectID `bson:"to"`
+	Type string             `bson:"type"`
+
+	ToOther   bool   `bson:"-"`
+	OtherName string `bson:"-"`
+}
